2dfa3c38aa6fce5256a54d162fc395bbbba86516
Jonas <jonas@firedev.tech>
2025-01-12 15:16:31 +0100

    start django backend

---
+++ server/aedify_personal_dispatching/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for aedify_personal_dispatching project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aedify_personal_dispatching.settings')
+
+application = get_asgi_application()
